263fa2dabc207f53bbb488854d292a8e90688506
rasool <kk0220091@gmail.com>
2023-10-12 23:18:25 +0500

    commit No 3

--- app/src/main/java/com/creative/shadow/text/name/appiskey_androidapp_task/extention/isNetworkAvailable.kt
+++ app/src/main/java/com/creative/shadow/text/name/appiskey_androidapp_task/extention/isNetworkAvailable.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
-fun isNetworkAvailable(context: Context): Boolean {
+fun Context.isNetworkAvailable(context: Context): Boolean {
     val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val network = connectivityManager.activeNetwork
     val capabilities = connectivityManager.getNetworkCapabilities(network)
